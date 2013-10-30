664f6276aa6114213b5f0c19ace8a49e0c33b3fe
Ryan J. Fork <rjfork@gmail.com>
2013-10-29 22:45:10 -0400

    Boom. Let's start this new - base pages are working. Navbar working on mobile. Finally.

---
+++ contact.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Contact - Modern Business - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">ryanfork.com</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="about.html">About</a></li>
+            <li><a href="http://blog.ryanfork.com">Blog</a></li>
+            <!--<li><a href="development.html">Development</a></li>-->
+            <li><a href="resume.html">Resume</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li class="active"><a href="contact.php">Contact</a></li>
+                <!--
+                <li><a href="full-width.html">Full Width Page</a></li>
+                <li><a href="sidebar.html">Sidebar Page</a></li>
+                <li><a href="faq.html">FAQ</a></li>
+                <li><a href="404.html">404</a></li>
+                <li><a href="pricing.html">Pricing Table</a></li>-->
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+
+    <div class="container">
+      
+      <div class="row">
+      
+        <div class="col-lg-12">
+          <h1 class="page-header">Contact <small>I'd Love to Hear From You!</small></h1>
+          <ol class="breadcrumb">
+            <li><a href="index.html">Home</a></li>
+            <li class="active">Contact</li>
+          </ol>
+        </div>
+        
+        <div class="col-lg-12">
+          <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
+          <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Georgia+Institute+of+Technology,+Atlanta,+GA&amp;aq=0&amp;oq=Georgia+&amp;sll=33.767713,-84.420604&amp;sspn=0.58279,0.472412&amp;ie=UTF8&amp;hq=Georgia+Institute+of+Technology,+Atlanta,+GA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+        </div>
+
+      </div><!-- /.row -->
+      
+      <div class="row">
+
+        <div class="col-sm-8">
+          <h3>Let's Get In Touch!</h3>
+          <p>I can be reached most days between 9am and 9pm. Feel free to contact me via e-mail and I'll get back to you as quick as possible.</p>
+			<?php  
+
+                // check for a successful form post  
+                if (isset($_GET['s'])) echo "<div class=\"alert alert-success\">".$_GET['s']."</div>";  
+          
+                // check for a form error  
+                elseif (isset($_GET['e'])) echo "<div class=\"alert alert-danger\">".$_GET['e']."</div>";  
+
+			?>
+            <form role="form" method="POST" action="contact-form-submission.php">
+              <div class="form-group col-lg-4">
+                <label for="input1">Name</label>
+                <input type="text" name="contact_name" class="form-control" id="input1">
+              </div>
+              <div class="form-group col-lg-4">
+                <label for="input2">Email Address</label>
+                <input type="email" name="contact_email" class="form-control" id="input2">
+              </div>
+              <div class="form-group col-lg-4">
+                <label for="input3">Phone Number</label>
+                <input type="phone" name="contact_phone" class="form-control" id="input3">
+              </div>
+              <div class="clearfix"></div>
+              <div class="form-group col-lg-12">
+                <label for="input4">Message</label>
+                <textarea name="contact_message" class="form-control" rows="6" id="input4"></textarea>
+              </div>
+              <div class="form-group col-lg-12">
+                <input type="hidden" name="save" value="contact">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+        </div>
+
+        <div class="col-sm-4">
+          <h3>Ryan Fork</h3>
+          <h4>Georgia Institute of Technology</h4>
+          <p>
+            330135 Georgia Tech Station<br>
+            Atlanta, GA 30332<br>
+          </p>
+          <p><i class="icon-phone"></i> <abbr title="Phone">P</abbr>: (724) 856-0525</p>
+          <p><i class="icon-envelope-alt"></i> <abbr title="Email">E</abbr>: <a href="mailto:ryan@ryanfork.com">ryan@ryanfork.com</a></p>
+          <p><i class="icon-time"></i> <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 9:00 PM</p>
+          <ul class="list-unstyled list-inline list-social-icons">
+            <li class="tooltip-social facebook-link"><a href="#facebook-page" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="icon-facebook-sign icon-2x"></i></a></li>
+            <li class="tooltip-social linkedin-link"><a href="#linkedin-company-page" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="icon-linkedin-sign icon-2x"></i></a></li>
+            <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="icon-twitter-sign icon-2x"></i></a></li>
+            <li class="tooltip-social google-plus-link"><a href="#google-plus-page" data-toggle="tooltip" data-placement="top" title="Google+"><i class="icon-google-plus-sign icon-2x"></i></a></li>
+          </ul>
+        </div>
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <div class="container">
+
+      <hr>
+
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            <center><p>Copyright &copy; Ryan Fork 2013</p></center>
+          </div>
+        </div>
+      </footer>
+
+    </div><!-- /.container -->
+    <!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/modern-business.js"></script>
+    
+  </body>
+</html>
